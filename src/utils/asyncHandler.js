ffe934b5304b4f22cd28592a5d3040a38558ac92
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-09-16 03:58:46 +0530

    utils custom api response , api error or asynhandler class for standarised production grade

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler = (requestHandler) => {
+
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+    }
+}
+
+
+
+export { asyncHandler }
+
+
+//higher order functions that ttake function as a parameter and are also can return the functions too 
+// const asyncHandler = (fn) => async (req,res,next) => { 
+
+//     try {
+//         await fn(req,res,next)
+//     } catch (err) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
